a70d7ed35b2a7de86b5eb9c25a3b5300e29f046e
jonysalgado <jonysalgadofilho@gmail.com>
2021-08-16 15:23:39 -0300

    lab 01

--- lab01.py
+++ lab01.py
@@ -14,12 +14,13 @@ warnings.filterwarnings("ignore")
 
 SHEET_URL = "16o3WCX6ULp16YCeNAdkAzZBG11yiEZjH"
 SHEET_NAME = "01_aircraft_survey.xlsx"
+POLY_DEGREE = 1
 
 # --------------------------------------------------------
 # Funções
 
 def download_sheet():
-    if "01_aircraft_survey.xlsx" in os.listdir():
+    if SHEET_NAME in os.listdir():
         answer = input("Você quer baixar novamente a planilha? (S,N) \n")
         if answer in ['S', 's']:
             os.system("rm 01_aircraft_survey.xlsx")
@@ -28,6 +29,49 @@ def download_sheet():
     else:
         print("Baixando a planilha ...")
         os.system("gdown --id {}".format(SHEET_URL))
+
+def fit_label(z):
+    name = "fit: $"
+    for i in range(len(z)-1, -1, -1):
+        if i > 1:
+            if z[i] > 0.001:
+                if name[-1] != "$" and z[i] > 0:
+                    name += "+ "
+                name += "{0:.3f}x^{1}".format(z[i], i)
+        elif i == 1:
+            if z[i] > 0.001:
+                if name[-1] != "$" and z[i] > 0:
+                    name += "+ "
+                name += "{0:.3f}x".format(z[i])
+        else:
+            if z[i] > 0.001:
+                if name[-1] != "$" and z[i] > 0:
+                    name += "+ "
+                name += "{0:.3f}".format(z[i])
+
+    name += "$"
+    return name
+
+def create_plot(X, y, title, path_name, xlabel, ylabel):
+    z = np.polyfit(X, y, POLY_DEGREE)
+    p = np.poly1d(z)
+    x_range = np.linspace(np.sort(X)[0], np.sort(X)[-1], 100)
+    y_pred = p(np.sort(X))
+    r_2 = r2_score(y, y_pred)
+
+    plt.figure(figsize=(12,8))
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.scatter(X, y, label="Dados experimentais")
+    name_fit = fit_label(z)
+    plt.plot(x_range, p(x_range), color='red', label=name_fit)
+    plt.legend()
+    plt.grid()
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(path_name)
+    plt.show()
 
 # --------------------------------------------------------
 # 1. Download planilha
@@ -49,57 +93,33 @@ print("Download Feito.")
 print("2. Processamento dos dados")
 df = pd.read_excel(SHEET_NAME)
 
+# --------------------------------------------------------
+# 3. Gerando os plots
+
+print("3. Gerando os plots")
 # Plot MTOW x Peso vazio
-
-X, y = df.iloc[:2, 1], df.iloc[:2, 2]
-z = np.polyfit(X, y, 2)
-p = np.poly1d(z)
-y_pred = p(np.sort(X))
-r_2 = r2_score(y, y_pred)
-
-plt.figure(figsize=(12,8))
-plt.scatter(X, y, label="fit")
-plt.plot(np.sort(X), y_pred, color='red', label="Dados experimentais")
-plt.grid(True)
-plt.legend()
-plt.title("Plot MTOW x Peso vazio | $R^2 = ${}".format(r_2))
-plt.xlabel(df.columns[1])
-plt.ylabel(df.columns[2])
-plt.show()
+create_plot(X=df.iloc[:, 1], 
+            y=df.iloc[:, 2], 
+            title="Plot MTOW x Peso vazio", 
+            path_name="MTOW_Peso_vazio.jpeg",
+            xlabel=df.columns[1],
+            ylabel=df.columns[2])
 
 
 # Plot MTOW x Número de passageiros
 
-X, y = df.iloc[:2, 1], df.iloc[:2, 4]
-z = np.polyfit(X, y, 2)
-p = np.poly1d(z)
-y_pred = p(np.sort(X))
-r_2 = r2_score(y, y_pred)
-
-plt.figure(figsize=(12,8))
-plt.scatter(X, y, label="fit")
-plt.plot(np.sort(X), y_pred, color='red', label="Dados experimentais")
-plt.grid(True)
-plt.legend()
-plt.title("Plot MTOW x Número de passageiros | $R^2 = ${}".format(r_2))
-plt.xlabel(df.columns[1])
-plt.ylabel(df.columns[4])
-plt.show()
+create_plot(X=df.iloc[:, 1], 
+            y=df.iloc[:, 4], 
+            title="Plot MTOW x Número de passageiros", 
+            path_name="MTOW_N_passageiros.jpeg",
+            xlabel=df.columns[1],
+            ylabel=df.columns[4])
 
 # Plot Número de passageiros × Comprimento da fuselagem
 
-X, y = df.iloc[:2, 32], df.iloc[:2, 4]
-z = np.polyfit(X, y, 2)
-p = np.poly1d(z)
-y_pred = p(np.sort(X))
-r_2 = r2_score(y, y_pred)
-
-plt.figure(figsize=(12,8))
-plt.scatter(X, y, label="fit")
-plt.plot(np.sort(X), y_pred, color='red', label="Dados experimentais")
-plt.grid(True)
-plt.legend()
-plt.title("Plot Número de passageiros × Comprimento da fuselagem | $R^2 = ${}".format(r_2))
-plt.xlabel(df.columns[32])
-plt.ylabel(df.columns[4])
-plt.show()
+create_plot(X=df.iloc[:, 35], 
+            y=df.iloc[:, 4], 
+            title="Plot Número de passageiros × Comprimento da fuselagem", 
+            path_name="N_passageiros_Comprimento_da_fuselagem.jpeg",
+            xlabel=df.columns[35],
+            ylabel=df.columns[4])
